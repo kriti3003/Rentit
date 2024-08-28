9cb249469f8a6d17e3fc023cd3bed0068d7199dd
kriti3003 <83621556+kriti3003@users.noreply.github.com>
2024-08-28 13:22:03 +0530

    NavBar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,123 @@
+import p_img1 from './p_img1.jpg'
+import p_img2_1 from './p_img2_1.jpg'
+import p_img2_2 from './p_img2_2.jpg'
+import p_img2_3 from './p_img2_3.jpg'
+import p_img2_4 from './p_img2_4.jpg'
+import p_img3 from './p_img3.jpg'
+import p_img4 from './p_img4.jpg'
+import p_img5 from './p_img5.jpg'
+import p_img6 from './p_img6.jpg'
+import p_img7 from './p_img7.jpg'
+import p_img8 from './p_img8.jpg'
+import p_img9 from './p_img9.jpg'
+import p_img10 from './p_img10.jpg'
+import p_img11 from './p_img11.jpg'
+import p_img12 from './p_img12.jpg'
+import p_img13 from './p_img13.jpg'
+import p_img14 from './p_img14.jpg'
+import p_img15 from './p_img15.jpg'
+import p_img16 from './p_img16.jpg'
+import p_img17 from './p_img17.jpg'
+import p_img18 from './p_img18.jpg'
+import p_img19 from './p_img19.jpg'
+import p_img20 from './p_img20.jpg'
+import p_img21 from './p_img21.jpg'
+import p_img22 from './p_img22.jpg'
+import p_img23 from './p_img23.jpg'
+import p_img24 from './p_img24.jpg'
+import p_img25 from './p_img25.jpg'
+import p_img26 from './p_img26.jpg'
+import p_img27 from './p_img27.jpg'
+import p_img28 from './p_img28.jpg'
+import p_img29 from './p_img29.jpg'
+import p_img30 from './p_img30.jpg'
+import p_img31 from './p_img31.jpg'
+import p_img32 from './p_img32.jpg'
+import p_img33 from './p_img33.jpg'
+import p_img34 from './p_img34.jpg'
+import p_img35 from './p_img35.jpg'
+import p_img36 from './p_img36.jpg'
+import p_img37 from './p_img37.jpg'
+import p_img38 from './p_img38.jpg'
+import p_img39 from './p_img39.jpg'
+import p_img40 from './p_img40.jpg'
+import p_img41 from './p_img41.jpg'
+import p_img42 from './p_img42.jpg'
+import p_img43 from './p_img43.jpg'
+import p_img44 from './p_img44.jpg'
+import p_img45 from './p_img45.jpg'
+import p_img46 from './p_img46.jpg'
+import p_img47 from './p_img47.jpg'
+import p_img48 from './p_img48.jpg'
+import p_img49 from './p_img49.jpg'
+import p_img50 from './p_img50.jpg'
+import p_img51 from './p_img51.jpg'
+import p_img52 from './p_img52.jpg'
+
+
+import logo from './logo.png'
+import hero_img from './hero_img.jpg'
+import cart_icon from './cart_icon.jpg'
+import bin_icon from './bin_icon.jpg'
+import dropdown_icon from './dropdown_icon.jpg'
+import exchange_icon from './exchange_icon.jpg'
+import profile_icon from './profile_icon.jpg'
+import quality_icon from './quality_icon.jpg'
+import search_icon from './search_icon.jpg'
+import star_dull_icon from './star_dull_icon.jpg'
+import star_icon from './star_icon.jpg'
+import support_icon from './support_icon.jpg'
+import menu_icon from './menu_icon.jpg'
+import about_icon from './about_icon.jpg'
+import contact_icon from './contact_icon.jpg'
+import razorpay_logo from './razorpay_logo.jpg'
+import stripe_logo from './stripe_logo.jpg'
+import cross_icon from './cross_icon.jpg'
+
+export const assets = {
+    logo,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_icon,
+    menu_icon,
+    about_icon,
+    contact_icon,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+};
+
+export const products = [
+    {
+        _id: "aaa",
+        name: "Women Top",
+        description: "a lightweight, usually knitted top",
+        price: 1000,
+        image: [p_img1],
+        category: "Women",
+        subCategory: "topwear",
+        sizes: ["S", "M", "L"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaab",
+        name: "men linen shirt",
+        description: "A lightweight, usually knitted shirt",
+        price: 2000,
+        image: [p_img2_1, p_img2_2, p_img2_3, p_img2_4],
+        category: "Men",
+        subCategory: "topwear",
+        sizes: ["M", "L", "XL"],
+        date: 1716621345448,
+        bestseller: true
+    }
+]
